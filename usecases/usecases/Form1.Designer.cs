692d09a433a2275908e350fe5867a28a901a149d
LAPTOP-FUP5QBKK\777beer <beer.baardse@gmail.com>
2017-02-23 23:09:27 +0100

    GUI Update rab / gboxes / panel resize

--- usecases/usecases/Form1.Designer.cs
+++ usecases/usecases/Form1.Designer.cs
@@ -28,20 +28,146 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.rabUseCase = new System.Windows.Forms.RadioButton();
+            this.rabActor = new System.Windows.Forms.RadioButton();
+            this.rabLine = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rabAdd = new System.Windows.Forms.RadioButton();
+            this.rabSelect = new System.Windows.Forms.RadioButton();
+            this.rabMove = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel1.Location = new System.Drawing.Point(12, 124);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(687, 355);
+            this.panel1.TabIndex = 0;
+            // 
+            // rabUseCase
+            // 
+            this.rabUseCase.AutoSize = true;
+            this.rabUseCase.Location = new System.Drawing.Point(6, 21);
+            this.rabUseCase.Name = "rabUseCase";
+            this.rabUseCase.Size = new System.Drawing.Size(86, 21);
+            this.rabUseCase.TabIndex = 1;
+            this.rabUseCase.TabStop = true;
+            this.rabUseCase.Text = "UseCase";
+            this.rabUseCase.UseVisualStyleBackColor = true;
+            // 
+            // rabActor
+            // 
+            this.rabActor.AutoSize = true;
+            this.rabActor.Location = new System.Drawing.Point(6, 45);
+            this.rabActor.Name = "rabActor";
+            this.rabActor.Size = new System.Drawing.Size(62, 21);
+            this.rabActor.TabIndex = 1;
+            this.rabActor.TabStop = true;
+            this.rabActor.Text = "Actor";
+            this.rabActor.UseVisualStyleBackColor = true;
+            // 
+            // rabLine
+            // 
+            this.rabLine.AutoSize = true;
+            this.rabLine.Location = new System.Drawing.Point(6, 72);
+            this.rabLine.Name = "rabLine";
+            this.rabLine.Size = new System.Drawing.Size(56, 21);
+            this.rabLine.TabIndex = 2;
+            this.rabLine.TabStop = true;
+            this.rabLine.Text = "Line";
+            this.rabLine.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rabUseCase);
+            this.groupBox1.Controls.Add(this.rabLine);
+            this.groupBox1.Controls.Add(this.rabActor);
+            this.groupBox1.Location = new System.Drawing.Point(12, 9);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(140, 109);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Elementen";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rabMove);
+            this.groupBox2.Controls.Add(this.rabSelect);
+            this.groupBox2.Controls.Add(this.rabAdd);
+            this.groupBox2.Location = new System.Drawing.Point(179, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 105);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Modes";
+            // 
+            // rabAdd
+            // 
+            this.rabAdd.AutoSize = true;
+            this.rabAdd.Location = new System.Drawing.Point(7, 22);
+            this.rabAdd.Name = "rabAdd";
+            this.rabAdd.Size = new System.Drawing.Size(54, 21);
+            this.rabAdd.TabIndex = 0;
+            this.rabAdd.TabStop = true;
+            this.rabAdd.Text = "Add";
+            this.rabAdd.UseVisualStyleBackColor = true;
+            // 
+            // rabSelect
+            // 
+            this.rabSelect.AutoSize = true;
+            this.rabSelect.Location = new System.Drawing.Point(7, 49);
+            this.rabSelect.Name = "rabSelect";
+            this.rabSelect.Size = new System.Drawing.Size(104, 21);
+            this.rabSelect.TabIndex = 0;
+            this.rabSelect.TabStop = true;
+            this.rabSelect.Text = "Select / Edit";
+            this.rabSelect.UseVisualStyleBackColor = true;
+            // 
+            // rabMove
+            // 
+            this.rabMove.AutoSize = true;
+            this.rabMove.Location = new System.Drawing.Point(7, 76);
+            this.rabMove.Name = "rabMove";
+            this.rabMove.Size = new System.Drawing.Size(63, 21);
+            this.rabMove.TabIndex = 0;
+            this.rabMove.TabStop = true;
+            this.rabMove.Text = "Move";
+            this.rabMove.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(571, 400);
+            this.ClientSize = new System.Drawing.Size(711, 491);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton rabUseCase;
+        private System.Windows.Forms.RadioButton rabActor;
+        private System.Windows.Forms.RadioButton rabLine;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton rabMove;
+        private System.Windows.Forms.RadioButton rabSelect;
+        private System.Windows.Forms.RadioButton rabAdd;
     }
 }
 
